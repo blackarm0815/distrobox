c0d84cb1f7c64072d0af94f472a9ee72c9c621b0
magnus <magnusmichaelson@protonmail.com>
2024-06-16 15:40:18 +0100

    basics for minecraft server

---
+++ minecraft_server/setup.sh
@@ -0,0 +1,9 @@
+sudo apt update
+sudo apt upgrade
+sudo apt install -y software-properties-common
+sudo add-apt-repository ppa:openjdk-r/ppa
+sudo apt update
+sudo apt install -y openjdk-17-jre-headless
+sudo apt install -y tmux
+sudo apt install -y ufw
+sudo ufw allow 25565
